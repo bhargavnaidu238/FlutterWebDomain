44eb7aedd64a562215040a8831d546b96721fbd7
bhargav.naidu238 <bhargav.naidu238@gmail.com>
2026-01-20 21:16:01 +0530

    Web Navigation Issue Fixed

--- hotel_booking_app/build/web/flutter_service_worker.js
+++ hotel_booking_app/build/web/flutter_service_worker.js
@@ -32,14 +32,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "a197fc77b7e63e995d2c0d139fbe81d8
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "d41d8cd98f00b204e9800998ecf8427e",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "8fbda8e8d1f956cdb77321635a21d66a",
+"flutter_bootstrap.js": "311fa7a8a81029654e08a945b17d78d2",
 "icons/Icon-192.png": "d41d8cd98f00b204e9800998ecf8427e",
 "icons/Icon-512.png": "d41d8cd98f00b204e9800998ecf8427e",
 "icons/Icon-maskable-192.png": "d41d8cd98f00b204e9800998ecf8427e",
 "icons/Icon-maskable-512.png": "d41d8cd98f00b204e9800998ecf8427e",
 "index.html": "83e0df64009e960b8cd20b0124446138",
 "/": "83e0df64009e960b8cd20b0124446138",
-"main.dart.js": "b3b35c4bf68b45f8d624b905425a6f38",
+"main.dart.js": "6bfeea4ddb274fd6585e7338b6785939",
 "manifest.json": "d4d8c49bc39b9de432aa3b669b994455",
 "version.json": "f9660db461eafb82589eff0cd456ec99"};
 // The application shell files that are downloaded before a service worker can
