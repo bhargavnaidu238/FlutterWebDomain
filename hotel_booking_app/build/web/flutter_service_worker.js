3fb94ad95e456f400cbc86059aceff00b71972df
bhargav.naidu238 <bhargav.naidu238@gmail.com>
2026-02-07 21:47:29 +0530

    Web Page Navigation Issue Fixed

--- hotel_booking_app/build/web/flutter_service_worker.js
+++ hotel_booking_app/build/web/flutter_service_worker.js
@@ -32,7 +32,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "a197fc77b7e63e995d2c0d139fbe81d8
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "d41d8cd98f00b204e9800998ecf8427e",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "34023f0115ecccb9b46c6724349de75f",
+"flutter_bootstrap.js": "076495cf6740116f2c270aaa061e821e",
 "icons/Icon-192.png": "d41d8cd98f00b204e9800998ecf8427e",
 "icons/Icon-512.png": "d41d8cd98f00b204e9800998ecf8427e",
 "icons/Icon-maskable-192.png": "d41d8cd98f00b204e9800998ecf8427e",
